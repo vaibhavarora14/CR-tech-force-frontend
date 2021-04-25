eefa1ecb97bc8ae983184fe54e514edf7779df8f
Vaibhav Arora <varora1406@gmail.com>
2021-04-25 15:51:06 +0530

    feat: 🎸 WIP :: integrate graphlquery with client for search

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './containers'
 import SearchPage from './containers/SearchPage'
 
+const httpLink = createHttpLink({
+    uri: 'https://vz3uy4iya2.execute-api.ap-south-1.amazonaws.com/'
+})
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
 function App() {
     return (
-        <div className="App">
-            <BrowserRouter>
-                <Header />
-                <div className="container">
-                    <Switch>
-                        <Route exact path="/home" component={Home} />
-                        <Route exact path="/search" component={SearchPage} />
-                        <Route exact path="/" component={Home} />
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        </div>
+        <ApolloProvider client={client}>
+            <div className="App">
+                <BrowserRouter>
+                    <Header />
+                    <div className="container">
+                        <Switch>
+                            <Route exact path="/home" component={Home} />
+                            <Route exact path="/search" component={SearchPage} />
+                            <Route exact path="/" component={Home} />
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </div>
+        </ApolloProvider>
     )
 }
 
--- src/containers/SearchPage/index.tsx
+++ src/containers/SearchPage/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useMutation, useQuery } from '@apollo/client'
+import gql from 'graphql-tag';
 
 function SearchPage() {
-    return (
-        <h3>
-            Search Page (coming soon...)
-        </h3>
-    )
+
+  const { loading, data } = useQuery(GET_SEARCH('Haryana', 'Ambala', 'Oxygen'))
+  console.log(data)
+
+  return (
+    <h3>
+      Search Page (coming soon...)
+    </h3>
+  )
+}
+
+const GET_SEARCH = (state: string, city: string, resourceType: string) => {
+  return gql`
+    query {
+        workspace {
+          tickets(filter: "custom_string:${state} AND custom_string:${city} AND custom_string:${resourceType}") {
+            edges {
+              node {
+                id
+                ticketId
+                supplierDonorName
+                supplierDonorContactNumber
+                city
+                state
+                costPerUnit
+                availableUnits
+              }
+            }
+          }
+        }
+      }
+    `
 }
 
 export default SearchPage
