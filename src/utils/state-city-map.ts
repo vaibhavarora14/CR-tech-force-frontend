e4bf89c9986418abcd58ea6cbe8810ed7e8460ea
Vaibhav Arora <varora1406@gmail.com>
2021-04-25 15:28:57 +0530

    feat: 🎸 add state city map base object

---
+++ src/utils/state-city-map.ts
@@ -0,0 +1,10 @@
+export const StateCityMap = [
+    {
+        state: 'Andhra Pradesh',
+        cities: ['Adoni', 'Amaravati', 'Anantapur']
+    },
+    {
+        state: 'Arunachal Pradesh',
+        cities: ['Itanagar']
+    }
+]
