ba1e2e09110015c0c28c3b53c7db428bbfe44f63
Vaibhav Arora <varora1406@gmail.com>
2021-04-27 03:59:44 +0530

    fix: 🐛 UI issues in card

--- src/components/SearchResultsCard/SearchResultCard.jsx
+++ src/components/SearchResultsCard/SearchResultCard.jsx
@@ -1,238 +1,311 @@
-import { Badge, Button, Card, IconButton, makeStyles, Snackbar, Typography, withTheme } from '@material-ui/core';
-import React, { useState } from 'react';
-import GreenTick from '../GreenTick/GreenTick';
-import ThumbsUp from '../../global/assets/icons/thumsup.svg';
-import ThumbsDown from '../../global/assets/icons/thumbsdown.svg';
-import { gql, useMutation } from '@apollo/client';
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-dayjs.extend(relativeTime)
+import {
+  Badge,
+  Button,
+  Card,
+  IconButton,
+  makeStyles,
+  Snackbar,
+  Typography,
+  withTheme,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import GreenTick from "../GreenTick/GreenTick";
+import ThumbsUp from "../../global/assets/icons/thumsup.svg";
+import ThumbsDown from "../../global/assets/icons/thumbsdown.svg";
+import { gql, useMutation } from "@apollo/client";
+import dayjs from "dayjs";
+import LinesEllipsis from "react-lines-ellipsis";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
-const useStyles = makeStyles(theme => ({
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        marginBottom: '1.5rem'
-    },
-    root: {
-        minWidth: theme.spacing(37.25),
-        border: '1px solid #F1F1F1',
-        background: 'linear-gradient(97.93deg, #4452CE 43.88%, #6744CC 109.61%)',
-    },
-    cardHeader: {
-        display: 'flex', 
-        flexDirection: 'column', 
-        padding: theme.spacing(3, 2.75),
-        color: '#fff'
-    },
-    cardTitle: {
-        lineHeight: '23.6px'
-    },
-    cardContent: {
-        display: 'flex', 
-        flexDirection: 'column', 
-        background: '#fff',
-        padding: theme.spacing(3, 4, 3.75, 3),
-        color: '#8F8F8F',
-        position: 'relative'
-    },
-    cardFooter: {
-        display: 'flex', 
-        flexDirection: 'row', 
-        alignItems: 'center',
-        padding: theme.spacing(2, 3),
-        color: '#fff'
-    },
-    thumbsUp: {
-        display: 'flex', 
-        alignItems: 'center', 
-        flexDirection: 'column',
-        marginLeft: theme.spacing(5)
-    },
-    thumbsDown: {
-        display: 'flex', 
-        alignItems: 'center', 
-        flexDirection: 'column', 
-        marginLeft: theme.spacing(2.5)
-    },
-    badge: {
-        marginTop: theme.spacing(-1),
-        marginRight: theme.spacing(-1),
-        color: '#000',
-        backgroundColor: '#fff'
-    },
-  }
-))
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    marginBottom: "1.5rem",
+  },
+  root: {
+    minWidth: theme.spacing(37.25),
+    border: "1px solid #F1F1F1",
+    background: "linear-gradient(97.93deg, #4452CE 43.88%, #6744CC 109.61%)",
+  },
+  cardHeader: {
+    display: "flex",
+    flexDirection: "column",
+    padding: theme.spacing(3, 2.75),
+    color: "#fff",
+  },
+  cardTitle: {
+    lineHeight: "23.6px",
+  },
+  cardContent: {
+    display: "flex",
+    flexDirection: "column",
+    background: "#fff",
+    padding: theme.spacing(3, 4, 3.75, 3),
+    color: "#8F8F8F",
+    position: "relative",
+  },
+  cardFooter: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: theme.spacing(2, 3),
+    color: "#fff",
+  },
+  thumbsUp: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    marginLeft: theme.spacing(5),
+  },
+  thumbsDown: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    marginLeft: theme.spacing(2.5),
+  },
+  badge: {
+    marginTop: theme.spacing(-1),
+    marginRight: theme.spacing(-1),
+    color: "#000",
+    backgroundColor: "#fff",
+  },
+}));
 
 const CHANGE_VOTE_COUNT = gql`
-    mutation (
-        $ticketId: String
-        $value: String
-    ) {
-        changeVoteCount(input: {
-            ticketId: $ticketId,
-            value: $value
-        }) {
-            status
-            message
-        }
+  mutation($ticketId: String, $value: String) {
+    changeVoteCount(input: { ticketId: $ticketId, value: $value }) {
+      status
+      message
     }
+  }
 `;
 
-
 const SearchResultCard = (props) => {
-    const classes = useStyles();
-    let { title, lastVerified, phone, location, details, thumbsUpcount, thumbsDownCount, theme, ticketId } = props;
+  const classes = useStyles();
+  let {
+    title,
+    lastVerified,
+    phone,
+    location,
+    details,
+    thumbsUpcount,
+    thumbsDownCount,
+    theme,
+    ticketId,
+  } = props;
 
-    if (thumbsUpcount && !isNaN(thumbsUpcount)) {
-        thumbsUpcount = parseInt(thumbsUpcount)
-    } else {
-        thumbsUpcount = 0
+  if (thumbsUpcount && !isNaN(thumbsUpcount)) {
+    thumbsUpcount = parseInt(thumbsUpcount);
+  } else {
+    thumbsUpcount = 0;
+  }
+
+  const [upvote, setUpvote] = useState(thumbsUpcount);
+
+  const [dialogMessage, setDialogMessage] = useState("");
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const [allowUpvote, setAllowUpvote] = useState(true);
+  const [allowDownvote, setAllowDownvote] = useState(true);
+
+  const [upvoteTicket] = useMutation(CHANGE_VOTE_COUNT, {
+    variables: {
+      ticketId,
+      value: `${thumbsUpcount + 1}`,
+    },
+    update(proxy, result) {
+      console.log(result);
+      if (
+        result &&
+        result.data &&
+        result.data.changeVoteCount &&
+        result.data.changeVoteCount.status === "200"
+      ) {
+        setUpvote(thumbsUpcount + 1);
+        setAllowDownvote(true);
+        setAllowUpvote(false);
+      } else {
+        setDialogMessage("Please try again later.");
+        setDialogOpen(true);
+      }
+    },
+    onError(err) {
+      setDialogMessage("Please try again later.");
+      setDialogOpen(true);
+    },
+  });
+
+  const [downvoteTicket] = useMutation(CHANGE_VOTE_COUNT, {
+    variables: {
+      ticketId,
+      value: `${upvote - 1 > -1 ? upvote - 1 : 0}`,
+    },
+    update(proxy, result) {
+      console.log(result);
+      if (
+        result &&
+        result.data &&
+        result.data.changeVoteCount &&
+        result.data.changeVoteCount.status === "200"
+      ) {
+        setUpvote(upvote - 1 > -1 ? upvote - 1 : 0);
+        setAllowDownvote(false);
+        setAllowUpvote(true);
+      } else {
+        setDialogMessage("Please try again later.");
+        setDialogOpen(true);
+      }
+    },
+    onError(err) {
+      setDialogMessage("Please try again later.");
+      setDialogOpen(true);
+    },
+  });
+
+  const copyInfo = () => {
+    const lastVerifiedText = `Last Verified: ${dayjs(lastVerified).fromNow()}`;
+    const phoneNumberText = `Phone Number - ${phone}`;
+    const addressText = `Address - ${location}`;
+    const detailsText = `Other details - ${details}`;
+
+    const copyText = `${title ? `${title} - ` : ""}${
+      lastVerified ? lastVerifiedText : ""
     }
+        ${phone ? phoneNumberText : ""}
+        ${location ? addressText : ""}
+        ${details ? detailsText : ""}`;
 
-    const [upvote, setUpvote] = useState(thumbsUpcount)
+    navigator.clipboard.writeText(copyText);
+    setDialogMessage("Information Copied to Clipboard");
+    setDialogOpen(true);
+  };
 
-    const [dialogMessage, setDialogMessage] = useState('')
-    const [dialogOpen, setDialogOpen] = useState(false)
+  const copyLink = () => {
+    // @TODO generate link to this page
+    navigator.clipboard.writeText(
+      "https://covid-resources-india-cef91.web.app/"
+    );
+    setDialogMessage("Link Copied to Clipboard");
+    setDialogOpen(true);
+  };
 
-    const [allowUpvote, setAllowUpvote] = useState(true)
-    const [allowDownvote, setAllowDownvote] = useState(true)
-
-    const [upvoteTicket] = useMutation(CHANGE_VOTE_COUNT, {
-        variables: {
-            ticketId,
-            value: `${(thumbsUpcount+1)}`,
-        },
-        update(proxy, result) {
-            console.log(result)
-            if (result && result.data && result.data.changeVoteCount && result.data.changeVoteCount.status === "200") {
-                setUpvote(thumbsUpcount + 1)
-                setAllowDownvote(true)
-                setAllowUpvote(false)
-            } else {
-                setDialogMessage("Please try again later.")
-                setDialogOpen(true)   
-            }
-        },
-        onError(err) {
-            setDialogMessage("Please try again later.")
-            setDialogOpen(true)
-        }
-    })
-
-
-    const [downvoteTicket] = useMutation(CHANGE_VOTE_COUNT, {
-        variables: {
-            ticketId,
-            value: `${(upvote - 1 > -1 ? upvote - 1 : 0)}`,
-        },
-        update(proxy, result) {
-            console.log(result)
-            if (result && result.data && result.data.changeVoteCount && result.data.changeVoteCount.status === "200") {
-                setUpvote(upvote - 1 > -1 ? upvote - 1 : 0)
-                setAllowDownvote(false)
-                setAllowUpvote(true)
-            } else {
-                setDialogMessage("Please try again later.")
-                setDialogOpen(true)   
-            }
-        },
-        onError(err) {
-            setDialogMessage("Please try again later.")
-            setDialogOpen(true)
-        }
-    })
-
-
-    const copyInfo = () => {
-        const lastVerifiedText = ` - Last Verified: ${lastVerified} ago`
-        const phoneNumberText = `Phone Number - ${phone}`
-        const addressText = `Address - ${location}`
-        const detailsText = `Other details - ${details}`
-
-        const copyText = 
-        `${title}${lastVerified ? lastVerifiedText : ''}
-        ${phone ? phoneNumberText : '' }
-        ${location ? addressText : '' }
-        ${details ? detailsText : '' }`
-
-        navigator.clipboard.writeText(copyText)
-        setDialogMessage("Information Copied to Clipboard")
-        setDialogOpen(true)   
-    }
-
-    const copyLink = () => {
-        // @TODO generate link to this page
-        navigator.clipboard.writeText("https://covid-resources-india-cef91.web.app/")
-        setDialogMessage("Link Copied to Clipboard")
-        setDialogOpen(true)   
-    }
-
-    return (
-        <div className={`${classes.container} ${props.className || ''}`}>
-            <Card variant='outlined' className={classes.root}>
-                <div className={classes.cardHeader}>
-                    <div style={{display: 'flex'}}>
-                        <Typography variant='body1'>{title}</Typography>
-                        <GreenTick />                        
-                    </div>
-                    <Typography style={{opacity: 0.7}} variant='body2'>Last Verified: {dayjs(lastVerified).fromNow()}</Typography>
-                </div>
-                
-                <div className={classes.cardContent}>
-                    <Typography variant='body2'>Phone</Typography>
-                    <Typography variant='h6'>{phone}</Typography>
-
-                    <Typography style={{marginTop: theme.spacing(2)}} variant='body2'>Location</Typography>
-                    <Typography variant='body1'>{location}</Typography>
-
-                    <Typography style={{marginTop: theme.spacing(2)}} variant='body2'>Details</Typography>
-                    <Typography variant='body1'>{details}</Typography>
-
-                    <Button onClick={() => copyInfo()} style={{
-                        position: 'absolute',
-                        top: theme.spacing(3.75),
-                        right: theme.spacing(3),
-                    }} color='primary' variant='outlined'>COPY</Button>
-                </div>
-
-                <div className={classes.cardFooter}>
-                    <Typography style={{opacity: 0.7}} variant='body1'>Was this helpful?</Typography>
-                    <div className={classes.thumbsUp}>
-                        <IconButton onClick={() => allowUpvote && upvoteTicket()} style={{background: '#cccccc'}}>
-                            <Badge classes={{badge: classes.badge}} color='secondary' badgeContent={upvote}>
-                                <img src={ThumbsUp} alt={"thumbs up"} />
-                            </Badge>
-                        </IconButton>
-                        <Typography style={{opacity: 0.7, marginTop: theme.spacing(0.25)}} variant='subtitle2'>Yes</Typography>
-                    </div>
-                    <div className={classes.thumbsDown}>
-                        <IconButton onClick={() => allowDownvote && downvoteTicket()} style={{background: '#cccccc'}}>
-                            <Badge classes={{badge: classes.badge}} color='secondary' badgeContent={thumbsDownCount}>
-                                <img src={ThumbsDown} alt={"thumbs down"} />
-                            </Badge>
-                        </IconButton>
-                        <Typography style={{opacity: 0.7, marginTop: theme.spacing(0.25)}} variant='subtitle2'>No</Typography>
-                    </div>
-                </div>
-            </Card>
-            
-            <Button onClick={() => copyLink()} color='primary' variant='outlined' style={{marginTop: theme.spacing(3)}}>
-                Share
-            </Button>
-
-            <Snackbar
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                }}
-                open={dialogOpen}
-                autoHideDuration={2000}
-                onClose={() => setDialogOpen(false)}
-                message={dialogMessage}
-            />
+  return (
+    <div className={`${classes.container} ${props.className || ""}`}>
+      <Card variant="outlined" className={classes.root}>
+        <div className={classes.cardHeader}>
+          <div className="d-flex mb-2">
+            <Typography variant="body1">{title}</Typography>
+            <GreenTick />
+          </div>
+          <Typography style={{ opacity: 0.7 }} variant="body2">
+            Last Verified: {dayjs(lastVerified).fromNow()}
+          </Typography>
         </div>
-    )
-}
 
-export default withTheme(SearchResultCard)
+        <div className={classes.cardContent}>
+          <div className="d-flex">
+            <div className="flex-grow-1 mr-3">
+              <Typography variant="body2">Phone</Typography>
+              <Typography variant="h6">{phone}</Typography>
+            </div>
+            <Button
+              style={{ height: "fit-content" }}
+              onClick={() => copyInfo()}
+              color="primary"
+              variant="outlined"
+            >
+              COPY
+            </Button>
+          </div>
+          <Typography style={{ marginTop: theme.spacing(2) }} variant="body2">
+            Location
+          </Typography>
+          <Typography variant="body1">{location}</Typography>
+
+          <Typography style={{ marginTop: theme.spacing(2) }} variant="body2">
+            Details
+          </Typography>
+          <Typography variant="body1">
+            <LinesEllipsis
+              text={details}
+              maxLine="2"
+              ellipsis="..."
+              trimRight
+              basedOn="letters"
+            />
+          </Typography>
+        </div>
+
+        <div className={classes.cardFooter}>
+          <Typography style={{ opacity: 0.7 }} variant="body1">
+            Was this helpful?
+          </Typography>
+          <div className={classes.thumbsUp}>
+            <IconButton
+              onClick={() => allowUpvote && upvoteTicket()}
+              style={{ background: "#cccccc" }}
+            >
+              <Badge
+                classes={{ badge: classes.badge }}
+                color="secondary"
+                badgeContent={upvote}
+              >
+                <img src={ThumbsUp} alt={"thumbs up"} />
+              </Badge>
+            </IconButton>
+            <Typography
+              style={{ opacity: 0.7, marginTop: theme.spacing(0.25) }}
+              variant="subtitle2"
+            >
+              Yes
+            </Typography>
+          </div>
+          <div className={classes.thumbsDown}>
+            <IconButton
+              onClick={() => allowDownvote && downvoteTicket()}
+              style={{ background: "#cccccc" }}
+            >
+              <Badge
+                classes={{ badge: classes.badge }}
+                color="secondary"
+                badgeContent={thumbsDownCount}
+              >
+                <img src={ThumbsDown} alt={"thumbs down"} />
+              </Badge>
+            </IconButton>
+            <Typography
+              style={{ opacity: 0.7, marginTop: theme.spacing(0.25) }}
+              variant="subtitle2"
+            >
+              No
+            </Typography>
+          </div>
+        </div>
+      </Card>
+
+      <Button
+        onClick={() => copyLink()}
+        color="primary"
+        variant="outlined"
+        style={{ marginTop: theme.spacing(3) }}
+      >
+        Share
+      </Button>
+
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        open={dialogOpen}
+        autoHideDuration={2000}
+        onClose={() => setDialogOpen(false)}
+        message={dialogMessage}
+      />
+    </div>
+  );
+};
+
+export default withTheme(SearchResultCard);
