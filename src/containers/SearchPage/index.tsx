cb24e8547b2069bfa64f4bc27df710d44c873f99
Vaibhav Arora <varora1406@gmail.com>
2021-04-26 14:36:54 +0530

    fix: 🐛 graphql empty set query + add loader

--- src/containers/SearchPage/index.tsx
+++ src/containers/SearchPage/index.tsx
@@ -5,13 +5,16 @@ import React, { useContext } from 'react';
 import SearchBar from '../../components/SearchBar';
 import SearchResultCard from '../../components/SearchResultsCard/SearchResultCard';
 import { Context as SearchContext } from './../../context/SearchContext';
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 
 
 function SearchPage() {
   const { state } = useContext(SearchContext);
 
   let currentData = [];
-  const { data, called } = useQuery(GET_SEARCH(state?.searchInputs?.state, state?.searchInputs?.city, state?.searchInputs?.requirement))
+  const { data, called, loading } = useQuery(GET_SEARCH(state?.searchInputs?.state, state?.searchInputs?.city, state?.searchInputs?.requirement))
   if (state?.searchInputs) {
     currentData = data?.workspace?.tickets?.edges || [];
   }
@@ -66,7 +69,16 @@ function SearchPage() {
     <div>
       <SearchBar />
       {state?.searchInputs && called && <>
-        {currentData.length === 0 && <h3>Sorry, No data available</h3>}
+        {loading && <div className="d-flex justify-content-center">
+          <Loader
+            type="ThreeDots"
+            color="#4452CE"
+            height={100}
+            width={100}
+            timeout={3000} //3 secs
+          />
+        </div>}
+        {!loading && currentData.length === 0 && <h3>Sorry, No data available</h3>}
         {currentData.length !== 0 && <>
           <Typography color="textSecondary" className="mb-4">Showing {currentData.length} Results</Typography>
           <div className="d-flex flex-wrap">
@@ -89,10 +101,23 @@ function SearchPage() {
 }
 
 const GET_SEARCH = (state: string, city: string, resourceType: string) => {
+  let filter = "";
+  if (state) {
+    filter += `custom_string:'${state}'`;
+  }
+
+  if (city) {
+    filter += `${filter.length > 0 ? ' AND ' : ''}custom_string:'${city}'`;
+  }
+
+  if (resourceType) {
+    filter += `${filter.length > 0 ? ' AND ' : ''}custom_string:'${resourceType}'`;
+  }
+
   return gql`
     query {
         workspace {
-          tickets(filter: "custom_string:'${state}' AND custom_string:'${city}' AND custom_string:'${resourceType}'") {
+          tickets(filter: "${filter}") {
             edges {
               node {
                 id
